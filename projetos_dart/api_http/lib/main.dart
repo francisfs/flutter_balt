6d5b85d8f579d76fc89bbf323804b2795150063f
FRANCISLEY <55454628+francisfs@users.noreply.github.com>
2023-01-20 20:32:50 -0300

    assync

---
+++ projetos_dart/api_http/lib/main.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  final future = http.get(Uri.https('www.exemple.com.', '/'));
+  future.then((response) {
+    if (response.statusCode == 200) {
+      print('pagina carregada com sucesso');
+    }
+    if (response.statusCode == 301) {
+      print('pagina movida permanentemente');
+    }
+    if (response.statusCode == 404) {
+      print('pagina não encontrada');
+    }
+    if (response.statusCode == 500) {
+      print('erro interno no servidor');
+    }
+  });
+}
